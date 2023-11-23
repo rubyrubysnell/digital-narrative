c3a23854bf486e3b368717e2a50b553939465dcb
rubyrubysnell <81196130+rubyrubysnell@users.noreply.github.com>
2023-11-23 16:28:12 +0000

    23/11/23

--- js/outside2.js
+++ js/outside2.js
@@ -6,6 +6,10 @@ const image = document.getElementById("second")
 
 let fading = false
 
+// initially there have been no clicks
+
+let clickState = 0
+
 // detect when the user scrolls the mouse wheel
 
 window.addEventListener('wheel', (event) => {
@@ -44,6 +48,50 @@ window.addEventListener('wheel', (event) => {
   }
 }
 )
+
+// alternative trigger - clicking
+
+// when the window is clicked...
+
+window.onclick = (event) => {
+
+  console.log("click")
+
+  // if not fading, do this:
+
+  if (!fading) {
+
+    // increase the click state
+
+    clickState++;
+
+    // if the click number is odd, trigger the down animation
+
+    if (clickState % 2 !== 0) {
+      console.log("even")
+      image.classList.add("down")
+      image.classList.remove("up")
+    }
+
+    // if the click number is even, trigger the up animation
+
+    else {
+      console.log("odd")
+      image.classList.add("up")
+      image.classList.remove("down")
+    }
+
+    // doing the above means that now there is a fade happening:
+
+    fading = true
+
+    // during the fade, prevent the function from triggering for 2 second
+
+    setTimeout(() => {
+      fading = false
+    }, 2000)
+  }
+}
 
 
 
