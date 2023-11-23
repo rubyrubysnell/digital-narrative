b12e77835d7d2fdcb978f951a1527290a2d6d8d5
rubyrubysnell <81196130+rubyrubysnell@users.noreply.github.com>
2023-11-23 14:01:15 +0000

    23/11/23

--- js/outside2.js
+++ js/outside2.js
@@ -2,26 +2,48 @@
 
 const image = document.getElementById("second")
 
+// initially there is no fade because the user hasn't scrolled yet
+
+let fading = false
+
 // detect when the user scrolls the mouse wheel
 
-window.addEventListener('wheel', function(event) {
+window.addEventListener('wheel', (event) => {
+
+  // if not fading, do this
+
+  if (!fading) {
 
     // if the wheel scrolls vertically above 0 (the starting position) then the user is scrolling down
 
     if (event.deltaY > 0) {
       console.log("scrolling down")
 
-        // if scrolling down, add css class to trigger the "down" animation
+      // if scrolling down, add css class to trigger the "down" animation
 
       image.classList.add("down")
       image.classList.remove("up")
 
-        // if not scrolling down (scrolling up), use css class to trigger the "up" animation
+      // if not scrolling down (scrolling up), use css class to trigger the "up" animation
 
     } else {
       console.log("scrolling up")
       image.classList.add("up")
       image.classList.remove("down")
     }
+
+    // doing the above means that now there is a fade happening:
+
+    fading = true
+
+    // during the fade, prevent the function from triggering for 2 seconds
+
+    setTimeout(() => {
+      fading = false
+    }, 2000)
   }
+}
 )
+
+
+
